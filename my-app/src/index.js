a8d4001114f3f414a1e17929256e4d604d05a6e6
JunyeonWon1008 <junyeon.won@aivenautics.com>
2024-12-17 15:37:42 +0900

    react state

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-import CommentList from './chapter_05/CommentList';
+import NotificationList from "./chapter_06/NotificiationList";
 
 //chapter 03
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -24,12 +24,20 @@ import CommentList from './chapter_05/CommentList';
 //   )}, 1000);
 
 //chapter 05
-const root = createRoot(document.getElementById('root'));
+// const root = createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <CommentList />
+//   </React.StrictMode>
+// )
+
+//chapter 06
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
