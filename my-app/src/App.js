8e9461ee51a2eef66b7c206bb1473df446020b91
JUNYEON WON <wnsdus1008@gmail.com>
2024-12-21 22:18:43 +0900

    react props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() {
+function Header(props) {
   return (
     <header>
       <h1>
-        <a href='/'>WEB</a>
+        <a href='/'>{props.title}</a>
       </h1>
     </header>
   );
 
 }
 
-function Nav() {
+function Nav(props) {
   return (
     <nav>
       <ol>
-        <li><a href='/read/1'>html</a></li>
-        <li><a href='/read/2'>css</a></li>
-        <li><a href='/read/3'>javascript</a></li>
+        {props.topics.map(topic => {
+          return (
+            <li key={topic.id}>
+            <a href={'/read/' + topic.id}>{topic.title}</a>
+            </li>
+          );
+        })}
       </ol>
     </nav>
-  )
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
 }
 
 function App() {
+  const topics = [
+    {id: 1, title: 'html', body: 'html is ...'},
+    {id: 2, title: 'css', body: 'css is ...'},
+    {id: 3, title: 'javascript', body: 'javascript is ...'},
+  ];
+
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcom" body="Hello, WEB"></Article>
     </div>
   );
 }
